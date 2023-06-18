69d8065a56c7114d939f1e772177f30a65fa0724
NadiaAmina <130190541+NadiaAmina@users.noreply.github.com>
2023-06-18 18:17:08 +0100

    Update script.R

--- script.R
+++ script.R
@@ -1,17 +1,64 @@
-source("require_packages.R")
-require_packages(c(
-  "xml2",
-  "httr"
-))
+#question 1
+library(xml2)
+library(httr)
+library(curl)
+library(tidyverse)
+library(dplyr)
+library(stringr)
+install.packages("tidytext")
+install.packages("textdata")
+library(tidytext)
+install.packages("wordcloud")
+library(wordcloud)
+#get feed from bbc 
+a <- GET("http://feeds.bbci.co.uk/news/rss.xml?edition=uk")
+file <- paste0("file",Sys.Date(), ".xml")
+curl_download("http://feeds.bbci.co.uk/news/rss.xml?edition=uk",file)
+#save feed data in xml
+xml <- readLines(file)
+xmlString <- paste0 (xml , collapse = "" )
+Ourxml <- read_xml (xmlString)
+Ourxml
+headlines_nodes <- xml_find_all(Ourxml, ".//item/title")
+headlines <- xml_text(headlines_nodes)
+headlines
+desc_nodes <- xml_find_all(Ourxml, ".//item/description")
+desc <- xml_text(desc_nodes)
+desc 
+data <- paste(headlines,desc)
+data
+writeLines(data)
+body <- data %>% str_remove_all("[:punct:]") %>% str_to_lower()
+body
+n = 1:length(data)
+df <- tibble(body, n)
+df
+#tokenize data
+tokens <- df %>% unnest_tokens(word, body)
+tokens
+tokens_filtered <-tokens %>%
+  anti_join(stop_words, by = "word")
+word_count <- tokens_filtered %>% count(word, sort=TRUE)
+#top 100 from data frame
+top_100 <- top_n ( word_count , 100 , wt = n)
+top_100
+#plot word cloud
+wordcloud( top_100$word , top_100 $n)
+
+nrc_sentiments <- get_sentiments("nrc")
+nrc_sentiments
+question_nrc_sentiments <- word_count %>% inner_join ( nrc_sentiments )
+question_nrc_sentiments
+View(question_nrc_sentiments)
+#plot nrc graph
+nrc_graph <- question_nrc_sentiments %>%
+  ggplot ( aes (x = sentiment , y = n , fill = sentiment )) +
+  geom_col ( show.legend = FALSE ) +
+  labs ( title = " Frequency of NRC sentiments on BBC website ")
+nrc_graph
+ggsave ( "nrc_ graph.pdf" , plot = nrc_graph , width = 8, height =
+           4.5)
 
 
-# HTTP GET Request
-a <- GET("https://github.com/skamper1/GitHub-Actions-R-Template")
 
-html <- read_html(rawToChar(a$content))
 
-forks_counter_element <- xml_find_first(html, ".//span[@id='repo-network-counter']")
-
-forks_counter <- xml_integer(forks_counter_element)
-
-forks_counter |>  as.character() |> paste(Sys.time()) |> writeLines(format(Sys.Date(), "%m-%Y.txt"))
